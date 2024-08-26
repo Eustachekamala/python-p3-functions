2d6e4242826f90850892d00fcd58993b1e8ae161
eustachekamala <eustachejared25@gmail.com>
2024-08-26 14:44:04 +0300

    I Used the def funtion to manipulate the object==variables

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+    
+greet("Guido")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+add(45,55)
 
 def halve(number):
-    pass
+    if number == float:
+        return None
+    return number/2
+
